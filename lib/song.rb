38742fa46fdd79e1cbd7707e498bf6fb74da31e1
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-25 20:03:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,11 +8,6 @@ end
 def self.new_by_filename(filename)
 song_name = filename.split(' - ')[1]
 artist = Artist.find_or_create_by_name(filename.split(' - ')[0])
-song = self.new
-#artist.add_song(song)
-#artist.save
-song.artist = artist
-song
 end
 
 end
